d3e5e2ad439c245b0fefd74cd4d2e4401842ea6a
Wilanda2 <yolanda.lopezdelarica@gmail.com>
2024-01-21 08:09:47 +0100

    Favorites done

--- src/js/renderShows.js
+++ src/js/renderShows.js
@@ -3,13 +3,13 @@ function renderShows(arrayShows, infoContainer) {
     let html = '';
     for (const show of arrayShows) {
         html += 
-        `<div class="show js-show-selected" id="${show.mal_id}">
+        `<li class="js-show-selected" id="${show.mal_id}">
             <img src="${show.images.jpg.image_url}" class="js-img" alt="${show.title}">
       
             <div>
-            <h3 class="title">${show.title}</h3>
+            <h3>${show.title}</h3>
             </div>
-        </div>`
+        </li>`
     }
     infoContainer.innerHTML = html;
     listenerShows();
