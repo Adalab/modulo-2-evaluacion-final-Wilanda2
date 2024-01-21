aba6001e329fa6ccda7da17ecb45d9e6a8d0777f
Wilanda2 <yolanda.lopezdelarica@gmail.com>
2024-01-21 08:44:40 +0100

    Favs Working

--- src/js/getDataApi.js
+++ src/js/getDataApi.js
@@ -3,7 +3,6 @@ function getDataApi(url) {
     .then((response) => response.json())
     .then((data) => {
       showsList = data.data;
-      console.log(showsList);
       //aquí estoy segura de que los datos han llegado, por eso renderizo aquí
       renderShows(showsList, containerGen);
     });
